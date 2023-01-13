97b987aac358b2c62ad90772077ac4d62e1c8494
user <user@WIN-0NDJOI5S6DS>
2023-01-13 11:55:03 +0900

    first commit

---
+++ src/test1/For.java
@@ -0,0 +1,20 @@
+package test1;
+
+public class For {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+		String ai = "アブラカタブラ";
+		
+		System.out.println("ここから出るには呪文を三回唱えなさい");
+		
+		for(int i = 0; i < 3; i++) {
+			System.out.println("勇者：" + ai);
+		}
+		
+		System.out.println("勇者はダンジョンから生還することができた。");
+
+	}
+
+}
---
+++ src/test1/For2.java
@@ -0,0 +1,12 @@
+package test1;
+
+public class For2 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+		
+
+	}
+
+}
